1ec7c3189c437579a88d683c5085be2731d63d14
Sidney Sousa <sidney.sousa@ifms.edu.br>
2022-09-27 20:00:49 -0400

    aplicando a classe abstrata

--- exemplo-polimorfismo/src/models/dao/UserPropertyDAO.ts
+++ exemplo-polimorfismo/src/models/dao/UserPropertyDAO.ts
@@ -1,51 +1,16 @@
-import { appendFileSync, readFileSync, writeFileSync } from 'fs'
-import { join } from 'path'
 import UserProperty from '../entities/UserProperty'
-import IUserPropertyDAO from './IUserPropertyDAO'
 
-export default class UserPropertyDAO implements IUserPropertyDAO {
-  private _userPropertyFilePath: string
-  private _properties: UserProperty[]
+export default abstract class UserPropertyDAO {
+  protected _userPropertyFilePath: string
+  protected _properties: UserProperty[]
 
-  constructor() {
-    this._userPropertyFilePath = join(
-      __dirname,
-      '..',
-      '..',
-      'data',
-      'user.properties'
-    )
-
+  constructor(userPropertyFilePath: string) {
+    this._userPropertyFilePath = userPropertyFilePath
     this._loadProperties()
   }
 
-  private _loadProperties() {
-    this._properties = []
-    const content = readFileSync(this._userPropertyFilePath, 'utf-8')
-    const lines = content.split('\n')
-    lines.forEach((l) => {
-      // ["email", "sidney@email.com"]
-      if (l && l.includes('=')) {
-        const aux = l.split('=')
-        const property: UserProperty = {
-          key: aux[0],
-          value: aux[1],
-        }
-
-        this._properties.push(property)
-      }
-    })
-  }
-
-  private _saveProperties() {
-    // Reseta o arquivo
-    writeFileSync(this._userPropertyFilePath, '')
-
-    this._properties.forEach((p) => {
-      const line = `${p.key}=${p.value}\n`
-      appendFileSync(this._userPropertyFilePath, line)
-    })
-  }
+  protected abstract _loadProperties(): void
+  protected abstract _saveProperties(): void
 
   set(userProperty: UserProperty) {
     const { key } = userProperty
--- exemplo-polimorfismo/src/models/dao/UserPropertyDefaultDAO.ts
+++ exemplo-polimorfismo/src/models/dao/UserPropertyDefaultDAO.ts
@@ -3,19 +3,19 @@ import { join } from 'path'
 import UserProperty from '../entities/UserProperty'
 import UserPropertyDAO from './UserPropertyDAO'
 
-export default class UserPropertyCsvDAO extends UserPropertyDAO {
+export default class UserPropertyDefaultDAO extends UserPropertyDAO {
   constructor() {
-    super(join(__dirname, '..', '..', 'data', 'user.csv'))
+    super(join(__dirname, '..', '..', 'data', 'user.properties'))
   }
 
   protected _loadProperties() {
     this._properties = []
     const content = readFileSync(this._userPropertyFilePath, 'utf-8')
     const lines = content.split('\n')
-    lines.slice(1).forEach((l) => {
+    lines.forEach((l) => {
       // ["email", "sidney@email.com"]
-      if (l && l.includes(',')) {
-        const aux = l.split(',')
+      if (l && l.includes('=')) {
+        const aux = l.split('=')
         const property: UserProperty = {
           key: aux[0],
           value: aux[1],
@@ -28,10 +28,10 @@ export default class UserPropertyCsvDAO extends UserPropertyDAO {
 
   protected _saveProperties() {
     // Reseta o arquivo
-    writeFileSync(this._userPropertyFilePath, 'key,value\n')
+    writeFileSync(this._userPropertyFilePath, '')
 
     this._properties.forEach((p) => {
-      const line = `${p.key},${p.value}\n`
+      const line = `${p.key}=${p.value}\n`
       appendFileSync(this._userPropertyFilePath, line)
     })
   }
